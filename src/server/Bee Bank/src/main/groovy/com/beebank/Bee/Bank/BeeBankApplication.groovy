dd6a3dbb1b443f21d17ecb598e18cb4402b9a058
Vinston Guillaume <vinstonguillaume@gmail.com>
2018-11-27 04:16:01 -0500

    spring io

---
+++ src/server/Bee Bank/src/main/groovy/com/beebank/Bee/Bank/BeeBankApplication.groovy
@@ -0,0 +1,12 @@
+package com.beebank.Bee.Bank
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BeeBankApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run BeeBankApplication, args
+	}
+}
